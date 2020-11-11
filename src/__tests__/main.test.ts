3fe6d8276185f995f0d922480309ec70a0aa174d
emreoezmen <emreykal@gmail.com>
2020-11-11 12:11:19 +0100

    Adds the test and class navigation
    Adds the test and file navigation.ts which contains the lookup table
    for the rover
    
    Signed-off-by: emreoezmen <emreykal@gmail.com>

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,5 +1,5 @@
-import { Rover, Direction } from '../rover';
-
+import { Rover } from '../rover';
+import { commandsLookup } from '../navigator';
 describe('Mars rover testing', () => {
   test('Mars rover should land to mars with given parameters ', () => {
     const rover = new Rover(1, 1, 'EAST');
@@ -9,5 +9,9 @@ describe('Mars rover testing', () => {
   test('A rover has a to toString method which returns its coords and direction', () => {
     const rover = new Rover(3, 6, 'WEST');
     expect(rover.getLocationAsString()).toEqual('(3, 6) WEST');
+  });
+
+  test('Navigator should be initialized', () => {
+    expect(commandsLookup).toBeTruthy();
   });
 });
---
+++ src/navigator.ts
@@ -0,0 +1,12 @@
+const commandsLookup = {
+  NORTHF: [0, 1],
+  NORTHB: [0, -1],
+  EASTF: [1, 0],
+  EASTB: [-1, 0],
+  SOUTHF: [0, -1],
+  SOUTHB: [0, 1],
+  WESTF: [-1, 0],
+  WESTB: [1, 0],
+};
+
+export { commandsLookup };
