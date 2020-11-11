fd3ef0b82db28625f39322696556c022807a7465
emreoezmen <emreykal@gmail.com>
2020-11-11 12:40:21 +0100

    Adds the test for the applyTurnCommand method
    Adds the tests for the applyTurnCommand method which
    turns the rover to left and right.
    
    Signed-off-by: emreoezmen <emreykal@gmail.com>

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -19,10 +19,27 @@ describe('Mars rover testing', () => {
   });
 
   describe('Mars rover and its utilizer should be created correctly', () => {
-    test('Rover should apply command the move forward ', () => {
+    test('Rover should apply the command F to move forward ', () => {
       const rover = new Rover(1, 1, 'EAST');
       rover.applyMoveCommand('F');
       expect(rover.getLocationAsString()).toEqual('(2, 1) EAST');
+    });
+    test('Rover should apply the command B to move backwards ', () => {
+      const rover = new Rover(1, 0, 'NORTH');
+      rover.applyMoveCommand('B');
+      expect(rover.getLocationAsString()).toEqual('(1, -1) NORTH');
+    });
+
+    test('Rover should apply the command L to to turn left ', () => {
+      const rover = new Rover(1, 0, 'NORTH');
+      rover.applyTurnCommand('L');
+      expect(rover.getLocationAsString()).toEqual('(1, -1) WEST');
+    });
+
+    test('Rover should apply the command R to to turn left ', () => {
+      const rover = new Rover(7, 8, 'EAST');
+      rover.applyTurnCommand('R');
+      expect(rover.getLocationAsString()).toEqual('(1, -1) SOUTH');
     });
   });
 });
